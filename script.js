f3ec12f9782a4ab58b9c36599c15cc6aa82bcbe2
Ayush-Rajput123 <131739732+Ayush-Rajput123@users.noreply.github.com>
2025-08-24 15:50:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+    let currentAudio = null;
+
+    sounds.forEach(sound => {
+      const btn = document.getElementById(sound);
+      btn.addEventListener('click', () => {
+        stopSound();
+        currentAudio = new Audio(`sounds/${sound}.mp3`);
+        currentAudio.play();
+      });
+    });
+
+    document.getElementById('stop').addEventListener('click', stopSound);
+
+    function stopSound() {
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+    }.
