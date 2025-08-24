37756784b70c421e5be7001db0d464cf11693020
Ayush-Rajput123 <131739732+Ayush-Rajput123@users.noreply.github.com>
2025-08-24 15:59:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
-  let currentAudio = null;
 
-  sounds.forEach(function(sound) {
-    const btn = document.getElementById(sound);
-    btn.addEventListener('click', function() {
-      stopSound();
-      currentAudio = new Audio("sounds/" + sound + ".mp3");
-      currentAudio.play();
-    });
-  });
-
-  document.getElementById('stop').addEventListener('click', stopSound);
-
-  function stopSound() {
-    if (currentAudio) {
-      currentAudio.pause();
-      currentAudio.currentTime = 0;
+    function stopAll() {
+      sounds.forEach(function(sound) {
+        const audio = document.getElementById(sound + "-audio");
+        audio.pause();
+        audio.currentTime = 0;
+      });
     }
-  }
+
+    sounds.forEach(function(sound) {
+      const btn = document.getElementById(sound);
+      btn.addEventListener('click', function() {
+        stopAll();
+        const audio = document.getElementById(sound + "-audio");
+        audio.play();
+      });
+    });
+
+    document.getElementById('stop').addEventListener('click', stopAll);
